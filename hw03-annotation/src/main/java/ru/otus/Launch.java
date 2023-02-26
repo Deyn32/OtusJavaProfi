2d9253acaadb6237c27d8daca7dd2e8059b730f9
Deyn32 <deyn32@yandex.ru>
2023-02-26 14:14:13 +0300

    [hw-03] ДЗ по аннотациям

--- hw03-annotation/src/main/java/ru/otus/Launch.java
+++ hw03-annotation/src/main/java/ru/otus/Launch.java
@@ -1,5 +1,6 @@
 package ru.otus;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.otus.annotation.After;
 import ru.otus.annotation.Before;
 import ru.otus.annotation.Test;
@@ -11,6 +12,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class Launch {
 
     private static int countSuccessTests = 0;
@@ -18,38 +20,37 @@ public class Launch {
 
     public static void main(String[] args) {
 
-        System.out.println("Запуск тестов для DemoClassTest...\n");
-        launchTest(DemoClassTest.class);
+        log.info("Запуск тестов для DemoClassTest...\n");
+        try {
+            launchTest(DemoClassTest.class);
+        } catch (Exception ex) {
+            log.error("Ошибка обработки методов класса DemoClass!");
+        }
     }
 
-    public static void launchTest(Class<?>  clazz) {
+    public static void launchTest(Class<?> clazz)
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
         Method[] methods = clazz.getDeclaredMethods();
         List<List<Method>> execPlan = addTestMethods(methods);
         addBeforeAndAfterMethods(methods, execPlan);
 
         for (List<Method> testList : execPlan) {
-            try {
-                final Object testObj = clazz.getConstructor().newInstance();
-                for (Method method : testList) {
-                    try {
-                        method.invoke(testObj);
-                    } catch (InvocationTargetException e) {
-                        Throwable ext = e.getCause();
-                        if (ext instanceof SuccessTestException) {
-                            System.out.println("Тест " + method.getName() + "': ПРОЙДЕН");
-                            countSuccessTests++;
-                        } else {
-                            System.out.println("Тест " + method.getName() + "': ПРОВАЛЕН");
-                            countFailedTests++;
-                        }
-                    } catch (ReflectiveOperationException e) {
-                        e.printStackTrace();
+
+            final Object testObj = clazz.getConstructor().newInstance();
+            for (Method method : testList) {
+                try {
+                    method.invoke(testObj);
+                } catch (InvocationTargetException e) {
+                    Throwable ext = e.getCause();
+                    if (ext instanceof SuccessTestException) {
+                        log.info("Тест " + method.getName() + "': ПРОЙДЕН");
+                        countSuccessTests++;
+                    } else {
+                        log.info("Тест " + method.getName() + "': ПРОВАЛЕН");
+                        countFailedTests++;
                     }
                 }
-            }
-            catch (ReflectiveOperationException e) {
-                e.printStackTrace();
             }
         }
         print();
@@ -57,7 +58,7 @@ public class Launch {
 
     private static List<List<Method>> addTestMethods(Method[] methods) {
         List<List<Method>> execPlan = new ArrayList<>();
-        for (Method method: methods) {
+        for (Method method : methods) {
             if (method.isAnnotationPresent(Test.class)) {
                 List<Method> testList = new ArrayList<>();
                 testList.add(method);
@@ -79,16 +80,16 @@ public class Launch {
     }
 
     private static void print() {
-        System.out.println(printResult());
+        log.info(printResult());
         countSuccessTests = 0;
         countFailedTests = 0;
     }
 
     private static String printResult() {
         return String.format("""
-                        Всего было запущено тестов: %d
-                        Пройдено тестов: %d
-                        Провалено тестов: %d
-                        """, (countSuccessTests + countFailedTests), countSuccessTests, countFailedTests);
+                Всего было запущено тестов: %d
+                Пройдено тестов: %d
+                Провалено тестов: %d
+                """, (countSuccessTests + countFailedTests), countSuccessTests, countFailedTests);
     }
 }
